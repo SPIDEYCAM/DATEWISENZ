b056cbd27e07f3b0975b47dcd1ff65d8e2740428
Vedant <vedantbajpai007@gmail.com>
2026-05-16 03:28:44 +0530

    implement navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+import Navitems from "./Navitems";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link href="/">
+                <div className="flex items-center gap-2.5 cursor-pointer">
+                    <Image
+                        src="/images/logo.svg"
+                        alt="logo"
+                        width={46}
+                        height={44}
+                    />
+                </div>
+            </Link>
+
+            <div className="flex items-center gap-6">
+                <Navitems />
+                <p>Login</p>
+
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ components/Navitems.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { cn } from "@/lib/utils"
+
+const navitems = [
+    { title: 'Home', href: '/home' },
+    { title: 'Pricing', href: '/subscription' },
+]
+const Navitems = () => {
+    const pathname = usePathname();
+
+
+    return (
+
+        <nav className="flex items-center gap-4">
+            {
+                navitems.map(({ title, href }) => (
+                    <Link href={href} key={title} className={cn(pathname === href && "text-primary font-semibold")}>
+                        {title}
+                    </Link>
+                ))
+            }
+        </nav>
+    )
+}
+
+export default Navitems
